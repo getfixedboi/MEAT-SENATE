cf25392fe419f7f088b7c8b51786a5cf5b9f8b02
getfixedboi <137700325+getfixedboi@users.noreply.github.com>
2024-11-14 17:38:18 +0300

    Оптимизация кода

--- Assets/Scripts/Items/PlayerStatictics.cs
+++ Assets/Scripts/Items/PlayerStatictics.cs
@@ -13,35 +13,45 @@ public class PlayerStatictics : MonoBehaviour
     public float BaseMovSpeed;
     public float JumpForce;
     #endregion
+
     #region params
     [Header("Parameters")]
     private float MaxHP = 100;
+
     public float maxHP
     {
-        get
-        {
-            return MaxHP;
-        }
+        get => MaxHP;
         set
         {
             MaxHP = value;
-            _healthText.text = СurrentHP.ToString() + "/" + MaxHP.ToString();
+            _healthText.text = $"{СurrentHP}/{MaxHP}";
         }
     }
+
     public float СurrentHP;
+
     private bool _invincibility = false;
-    [SerializeField][Range(0.1f, 40f)] private float _invincibilityPeriod;
-    private static HashSet<ItemBehaviour> _playerItems;
-    private static HashSet<ModifierBehaviour> _playerModifiers;
+
+    [SerializeField]
+    [Range(0.1f, 40f)]
+    private float _invincibilityPeriod;
+
+    private static HashSet<ItemBehaviour> _playerItems = new();
+    private static HashSet<ModifierBehaviour> _playerModifiers = new();
     #endregion
+
     [Header("UI")]
+
     [SerializeField] private Canvas _playerCanvas;
     [SerializeField] private GameObject _itemImagePrefab;
     [SerializeField] private GameObject _modifierImagePrefab;
     [SerializeField] private UnityEngine.UI.Text _healthText;
-    private static List<GameObject> _itemUiPrefabs = new List<GameObject>();
-    private static List<GameObject> _modifierUiPrefabs = new List<GameObject>();
+
+    private static List<GameObject> _itemUiPrefabs = new();
+    private static List<GameObject> _modifierUiPrefabs = new();
+
     public static ShowModifierDescOnUI CurrentModifier;
+
     public GameObject ItemGrid;
     public GameObject ModGrid;
 
@@ -50,15 +60,13 @@ public class PlayerStatictics : MonoBehaviour
     private void Awake()
     {
         Instance = this.gameObject;
-        _playerItems = new HashSet<ItemBehaviour>();
-        _playerModifiers = new HashSet<ModifierBehaviour>();
         СurrentHP = MaxHP;
-
-        _healthText.text = СurrentHP.ToString() + "/" + MaxHP.ToString();
+        _healthText.text = $"{СurrentHP}/{MaxHP}";
     }
+
     private void Update()
     {
-        //Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН Я ДАУН 
+        // Логика обновления, если необходима
     }
 
     public void Heal(float healAmount)
@@ -68,53 +76,46 @@ public class PlayerStatictics : MonoBehaviour
         {
             СurrentHP = MaxHP;
         }
-        _healthText.text = СurrentHP.ToString() + "/" + MaxHP.ToString();
+        _healthText.text = $"{СurrentHP}/{MaxHP}";
     }
 
     public void TakeDamage(float inflictedDamage)
     {
-        if (_invincibility) { return; }
+        if (_invincibility) return;
 
         СurrentHP -= inflictedDamage;
-
-        PlayerProgress.ReceivedDamage += inflictedDamage;//
-        _healthText.text = СurrentHP.ToString() + "/" + MaxHP.ToString();
+        PlayerProgress.ReceivedDamage += inflictedDamage;
         StartCoroutine(C_TemporaryInvinsibility());
-
     }
+
     private IEnumerator C_TemporaryInvinsibility()
     {
         _invincibility = true;
         yield return new WaitForSeconds(_invincibilityPeriod);
         _invincibility = false;
     }
+
     #region items management
     public void AddItem(ItemBehaviour item)
     {
-        //Debugging("before add");
         if (!_playerItems.Contains(item))
         {
             TakeFromGroundItem(item.gameObject);
-
             _playerItems.Add(item);
             AddItemUI(item);
-
         }
         else
         {
             throw new ArgumentException($"Item {item.name} is already taken");
         }
-        //Debugging("after add");
     }
+
     public void AddItem(ItemBehaviour item, bool param)
     {
-        //Debugging("before add");
         if (!_playerItems.Contains(item))
         {
-            if (param)
-            {
-                item.gameObject.GetComponent<MeshRenderer>().enabled = false;
-            }
+            if (param) item.gameObject.GetComponent<MeshRenderer>().enabled = false;
+
             if (!item.isShop)
             {
                 TakeFromGroundItem(item.gameObject, param);
@@ -122,63 +123,42 @@ public class PlayerStatictics : MonoBehaviour
             else
             {
                 item.isShop = false;
-                // Делает item дочерним объектом игрока
                 item.transform.SetParent(transform);
-
-                // Перемещаем item к позиции игрока (можно добавить смещение при необходимости)
                 item.transform.localPosition = Vector3.zero;
 
-                if (param)
-                {
-                    item.gameObject.GetComponent<MeshRenderer>().enabled = true;
-                }
+                if (param) item.gameObject.GetComponent<MeshRenderer>().enabled = true;
 
-                if (item.TryGetComponent<Rigidbody>(out Rigidbody rb))
-                {
-                    rb.isKinematic = true;
-                }
+                if (item.TryGetComponent<Rigidbody>(out Rigidbody rb)) rb.isKinematic = true;
+                if (item.TryGetComponent<Collider>(out Collider col)) col.enabled = false;
 
-                // Отключаем коллайдер предмета
-                if (item.TryGetComponent<Collider>(out Collider col))
-                {
-                    col.enabled = false;
-                }
-
-                // Отключаем item, чтобы он не был видимым или интерактивным в мире
                 item.gameObject.SetActive(false);
             }
             _playerItems.Add(item);
             AddItemUI(item);
-
         }
         else
         {
             throw new ArgumentException($"Item {item.name} is already taken");
         }
-        //Debugging("after add");
     }
+
     public void RemoveItem(ItemBehaviour item)
     {
-        //Debugging("before revome");
         if (_playerItems.Contains(item))
         {
             RemoveItemUI(item);
-
             ThrowAwayItem(item.gameObject);
         }
         else
         {
-            throw new ArgumentException($"Item {item.name} does not exists");
+            throw new ArgumentException($"Item {item.name} does not exist");
         }
-        //Debugging("after revome");
     }
+
     private void AddItemUI(ItemBehaviour item)
     {
-
-        // Создаем новый экземпляр префаба
         GameObject gameObj = Instantiate(_itemImagePrefab, _playerCanvas.transform.position, Quaternion.identity);
 
-        // Настраиваем компонент ShowItemDescOnUI
         var showItemDesc = gameObj.GetComponent<ShowItemDescOnUI>();
         if (showItemDesc != null)
         {
@@ -186,31 +166,31 @@ public class PlayerStatictics : MonoBehaviour
             showItemDesc.ItemRef = item.GetComponent<ItemBehaviour>();
         }
 
-        // Устанавливаем родителя для позиционирования UI элемента
         gameObj.transform.SetParent(ItemGrid.transform, false);
 
-        // Добавляем созданный экземпляр в список для дальнейшего использования
         _itemUiPrefabs.Add(gameObj);
     }
-
 
     private void RemoveItemUI(ItemBehaviour item)
     {
         List<ItemBehaviour> list = _playerItems.ToList();
 
         GameObject tempItem = _itemUiPrefabs[list.IndexOf(item)];
+
         _itemUiPrefabs.Remove(tempItem);
         _playerItems.Remove(item);
+
         Destroy(tempItem);
     }
+
     #endregion
+
     #region modifiers management
     public void AddModifier(ModifierBehaviour item)
     {
         if (!_playerModifiers.Contains(item))
         {
             TakeFromGroundItem(item.gameObject);
-
             _playerModifiers.Add(item);
             ReloadModifierUI();
         }
@@ -219,16 +199,14 @@ public class PlayerStatictics : MonoBehaviour
             throw new ArgumentException($"Modifier {item.name} is already taken");
         }
     }
+
     public void AddModifier(ModifierBehaviour item, bool param)
     {
         if (!_playerModifiers.Contains(item))
         {
-            if (param)
-            {
-                item.gameObject.GetComponent<MeshRenderer>().enabled = false;
-            }
-            TakeFromGroundItem(item.gameObject, param);
+            if (param) item.gameObject.GetComponent<MeshRenderer>().enabled = false;
 
+            TakeFromGroundItem(item.gameObject, param);
             _playerModifiers.Add(item);
             ReloadModifierUI();
         }
@@ -237,173 +215,112 @@ public class PlayerStatictics : MonoBehaviour
             throw new ArgumentException($"Modifier {item.name} is already taken");
         }
     }
+
     private void ReloadModifierUI()
     {
-        // Удаляем старые UI элементы
         for (int i = 0; i < _modifierUiPrefabs.Count; i++)
         {
             Destroy(_modifierUiPrefabs[i]);
         }
+
         _modifierUiPrefabs.Clear();
 
         foreach (ModifierBehaviour mod in _playerModifiers)
         {
-
-            // Создаем новый экземпляр префаба
             GameObject gameObj = Instantiate(_modifierImagePrefab, _playerCanvas.transform.position, Quaternion.identity);
 
-            // Добавляем компонент только к экземпляру
-            ModifierBehaviour modRef = mod.GetComponentInChildren<ModifierBehaviour>();
+            var modRef = mod.GetComponentInChildren<ModifierBehaviour>();
             gameObj.GetComponent<ShowModifierDescOnUI>().ModRef = modRef;
 
-            // Настраиваем компонент ShowModDescOnUI
             var showModDesc = gameObj.GetComponent<ShowModifierDescOnUI>();
             if (showModDesc != null)
             {
                 showModDesc.Canvas = _playerCanvas;
             }
 
-            // Устанавливаем родителя для позиционирования UI элемента
             gameObj.transform.SetParent(ModGrid.transform, false);
 
-            // Добавляем созданный экземпляр в список для дальнейшего использования
             _modifierUiPrefabs.Add(gameObj);
 
-            if (modRef == null)
-            {
-                UnityEngine.Debug.Log("Не удалось скопировать ссылку на компонент");
-
-                modRef = mod.GetComponentInChildren<ModifierBehaviour>();
-                if (modRef == null)
-                {
-                    UnityEngine.Debug.Log("Не удается снять ссылку с предмета");
-                }
-            }
-            else if (gameObj.GetComponent<ShowModifierDescOnUI>().ModRef == null)
-            {
-                UnityEngine.Debug.Log("Ссылка по прежнему не была установлена");
-            }
+            if (modRef == null) Debug.Log("Не удалось скопировать ссылку на компонент");
         }
     }
     #endregion
+
     private void TakeFromGroundItem(GameObject item)
     {
-        // Отключаем физику у предмета, если она есть, чтобы она не мешала движению
-        if (item.TryGetComponent<Rigidbody>(out Rigidbody rb))
-        {
-            rb.isKinematic = true;
-        }
+        if (item.TryGetComponent<Rigidbody>(out Rigidbody rb)) rb.isKinematic = true;
+        if (item.TryGetComponent<Collider>(out Collider col)) col.enabled = false;
 
-        // Отключаем коллайдер предмета
-        if (item.TryGetComponent<Collider>(out Collider col))
-        {
-            col.enabled = false;
-        }
-
-        // Запускаем корутину для динамического перемещения предмета к игроку
         StartCoroutine(AnimateItemAbsorption(item));
     }
+
     private void TakeFromGroundItem(GameObject item, bool param)
     {
-        // Отключаем физику у предмета, если она есть, чтобы она не мешала движению
-        if (item.TryGetComponent<Rigidbody>(out Rigidbody rb))
-        {
-            rb.isKinematic = true;
-        }
+        if (item.TryGetComponent<Rigidbody>(out Rigidbody rb)) rb.isKinematic = true;
+        if (item.TryGetComponent<Collider>(out Collider col)) col.enabled = false;
 
-        // Отключаем коллайдер предмета
-        if (item.TryGetComponent<Collider>(out Collider col))
-        {
-            col.enabled = false;
-        }
-
-        // Запускаем корутину для динамического перемещения предмета к игроку
         StartCoroutine(AnimateItemAbsorption(item, param));
     }
+
     private IEnumerator AnimateItemAbsorption(GameObject item)
     {
-        float speed = 20f; // Скорость перемещения предмета к игроку
-
-        // Целевая позиция для предмета (например, на уровне "живота", чуть ниже камеры)
+        float speed = 20f;
         Vector3 targetPosition = transform.position + transform.forward * 0.5f - transform.up * .4f;
 
         while (Vector3.Distance(item.transform.position, targetPosition) > 0.1f)
         {
-            // Динамическое движение предмета к цели с заданной скоростью
             item.transform.position = Vector3.MoveTowards(item.transform.position, targetPosition, speed * Time.deltaTime);
-            yield return null; // Ожидаем до следующего кадра
+            yield return null;
         }
 
-        // Убедимся, что предмет точно находится в целевой позиции
         item.transform.position = targetPosition;
-
-        // Делает item дочерним объектом игрока
         item.transform.SetParent(transform);
-
-        // Перемещаем item к позиции игрока (можно добавить смещение при необходимости)
         item.transform.localPosition = Vector3.zero;
-
-        // Отключаем item, чтобы он не был видимым или интерактивным в мире
         item.SetActive(false);
     }
+
     private IEnumerator AnimateItemAbsorption(GameObject item, bool param)
     {
-        float speed = 20f; // Скорость перемещения предмета к игроку
-
-        // Целевая позиция для предмета (например, на уровне "живота", чуть ниже камеры)
+        float speed = 20f;
         Vector3 targetPosition = transform.position + transform.forward * 0.5f - transform.up * .4f;
 
         while (Vector3.Distance(item.transform.position, targetPosition) > 0.1f)
         {
-            // Динамическое движение предмета к цели с заданной скоростью
             item.transform.position = Vector3.MoveTowards(item.transform.position, targetPosition, speed * Time.deltaTime);
-            yield return null; // Ожидаем до следующего кадра
+            yield return null;
         }
 
-        // Убедимся, что предмет точно находится в целевой позиции
         item.transform.position = targetPosition;
-
-        // Делает item дочерним объектом игрока
         item.transform.SetParent(transform);
-
-        // Перемещаем item к позиции игрока (можно добавить смещение при необходимости)
         item.transform.localPosition = Vector3.zero;
 
-        if (param)
-        {
-            item.gameObject.GetComponent<MeshRenderer>().enabled = true;
-        }
+        if (param) item.gameObject.GetComponent<MeshRenderer>().enabled = true;
 
-        // Отключаем item, чтобы он не был видимым или интерактивным в мире
         item.SetActive(false);
     }
+
     private void ThrowAwayItem(GameObject item)
     {
-        // Включаем item, чтобы он снова был видимым и интерактивным
         item.SetActive(true);
 
-        // Удаляем item из дочерних объектов игрока
         item.transform.SetParent(null);
 
-        // Устанавливаем позицию выбрасывания перед игроком
         item.transform.position = transform.position + transform.forward;
 
-        // Включаем физику для предмета
         if (item.TryGetComponent<Rigidbody>(out Rigidbody rb))
         {
             rb.isKinematic = false;
-            // Добавляем силу для "выброса" предмета вперед
             rb.AddForce(transform.forward * 5f, ForceMode.Impulse);
         }
 
-        // Запускаем корутину для включения коллайдера с задержкой
-        StartCoroutine(EnableColliderAfterDelay(item, 0.1f)); // Задержка в 0.1 секунды
+        StartCoroutine(EnableColliderAfterDelay(item, 0.1f));
     }
+
     private IEnumerator EnableColliderAfterDelay(GameObject item, float delay)
     {
         yield return new WaitForSeconds(delay);
 
-        // Включаем коллайдер предмета после задержки
         if (item.TryGetComponent<Collider>(out Collider col))
         {
             col.enabled = true;
@@ -413,6 +330,7 @@ public class PlayerStatictics : MonoBehaviour
     private void Debugging(string startMes)
     {
         Debug.Log(startMes);
+
         foreach (var v in _playerItems)
         {
             Debug.LogWarning(v.name);
